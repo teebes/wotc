636edd60b1cd80932b0299e44cd6cc9ff6444dec
Thibaud Morel <teebes@gmail.com>
2018-09-12 08:23:22 -0400

    I know nothing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <Client/>
+    <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Client from './components/Client.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Client
+  },
+  created: function() {
+    console.log('vue app initialized');
   }
 }
 </script>
---
+++ src/components/Client.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Login/>
+    </div>
+</template>
+
+<script>
+import Login from './Login.vue'
+
+export default {
+    name: 'Client',
+    components: {
+        Login,
+    }
+}
+</script>
